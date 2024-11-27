c623b67044ee7b08893b5925b42d1fecef8f6cb3
kyunmilee <lkmin6943@naver.com>
2024-11-27 03:00:16 +0000

    Feat: data imputation function
    #29

--- src/models/fm_model.py
+++ src/models/fm_model.py
@@ -46,10 +46,12 @@ def director_encoder(data):
     return data, encoder_director
 
 
-def merge_data(train_ratings, genres, writers, directors, encode_item):
+def merge_data(train_ratings, genres, writers, directors, years, titles, encode_item):
     genres["item"] = encode_item.transform(genres["item"])
     writers["item"] = encode_item.transform(writers["item"])
     directors["item"] = encode_item.transform(directors["item"])
+    years["item"] = encode_item.transform(years["item"])
+    titles["item"] = encode_item.transform(titles["item"])
     genrelist = genres.groupby("item")["genre"].apply(list)
     writerlist = writers.groupby("item")["writer"].apply(list)
     directorlist = directors.groupby("item")["director"].apply(list)
@@ -57,15 +59,33 @@ def merge_data(train_ratings, genres, writers, directors, encode_item):
     df1 = pd.merge(train_ratings, genrelist, on="item", how="left")
     df2 = pd.merge(df1, writerlist, on="item", how="left")
     df3 = pd.merge(df2, directorlist, on="item", how="left")
+    df4 = pd.merge(df3, years, on="item", how="left")
+    df5 = pd.merge(df4, titles, on="item", how="left")
 
-    return df3
+    return df5
+
+
+def data_fillna(df):
+
+    df["extract_year"] = df["title"].str.extract(r"\((\d{4})")[0]
+    df["year"] = df["year"].fillna(df["extract_year"].astype("float64"))
+    df["year"] = df["year"].astype("int32")
+    df["year"] = (df["year"] - 1922) / (2014 - 1920)
+    df.drop(columns=["time"], inplace=True)
+    df.drop(columns=["title"], inplace=True)
+    df.drop(columns=["extract_year"], inplace=True)
+    df = df.fillna("unknown")
+    df["director"] = df["director"].apply(lambda x: [] if x == "unknown" else x)
+    df["writer"] = df["writer"].apply(lambda x: [] if x == "unknown" else x)
+
+    return df
 
 
 def preprocess_for_fm(data_path):
     pass
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     data_path = "/data/ephemeral/home/lee/data/train/"
     train_rating, title, year, director, genre, writer = data_load(data_path)
     train_ratings, titles, years, directors, genres, writers = (
@@ -76,8 +96,12 @@ if __name__ == "main":
         genre.copy(),
         writer.copy(),
     )
+
+    # encoder
     train_ratings, encode_user, encode_item = user_and_item_encoder(train_ratings)
     genres, encoder_genre = genre_encoder(genres)
     writers, encoder_writer = writer_encoder(writers)
     directors, encoder_director = director_encoder(directors)
-    df = merge_data(train_ratings, genres, writers, directors, encode_item)
+    df = merge_data(train_ratings, genres, writers, directors, years, titles, encode_item)
+    df = data_fillna(df)
+    print(df.head())
