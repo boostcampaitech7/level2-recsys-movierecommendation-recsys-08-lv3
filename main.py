f81d70d7a5c360ed86c7523a5218ae2069abd808
SEUNGHUN SHIN <sinseunghun708@gmail.com>
2024-12-01 22:53:16 +0900

    Refactor: modify main

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ def main(args):
     seed_everything(args)
     
     datatype = args.model_args[args.model].datatype
+
+    if datatype == 'basic':
+        args.predict=True
     # data load
+    else:
+        data_split_fn = getattr(data_module, f'{datatype}_data_split')  # e.g. basic_data_split()
     data_load_fn = getattr(data_module, f'{datatype}_data_load')  # e.g. basic_data_load()
-    data_split_fn = getattr(data_module, f'{datatype}_data_split')  # e.g. basic_data_split()
     data = data_load_fn(args)
-    print(data.keys())
+
     if datatype == 'context':
         data_sideinfo = getattr(data_module,f'{datatype}_data_sideinfo')
         data_sidemerge = getattr(data_module, f'{datatype}_data_side_merge')
@@ -24,37 +28,38 @@ def main(args):
         data = data_sideinfo(args,data)
 
     model_name = args.model
-    trainer_assigned = getattr(train_module, f'{model_name}Trainer')
     print(model_name)
+    print(args.predict)
+    trainer_assigned = getattr(train_module, f'{model_name}Trainer')
+    
     # data_ side
     if not args.predict:
         # train
         print('---split---')
         data= data_split_fn(args,data)
-        print('train_data: ',data['train'])
-        print('valid_data: ',data['valid'])
+
         if datatype=='context':
             print('---sideinf merging---')
             data['train'] = sample_negative_items(data['train'], args.seed, args.negative)
             data=data_sidemerge(args,data)
-        print('train_data: ',data['train'])
-        print('valid_data: ',data['valid'])
-        #train
-        print('---training---')
+
         trainer =trainer_assigned(args,data)
         trained_model = trainer.train_model()
         print('---end---')
     else:
-        # train
+        # context data
         if datatype=='context':
             data['total'] = sample_negative_items(data['total'], args.seed, args.negative_samples)
             data=data_sidemerge(args,data)
-        #train
-        trainer= trainer_assigned(args,data)
-        trained_model = trainer.train_model()
-        predict_base = trainer.generate_prediction_base()
-        predictions = trainer.evaluate(trained_model, predict_base, top_k=10)
-        
+            trainer= trainer_assigned(args,data)
+            trained_model = trainer.train_model()
+            predict_base = trainer.generate_prediction_base()
+            predictions = trainer.evaluate(trained_model, predict_base, top_k=10)
+        else:
+            trainer= trainer_assigned(args,data)
+            trained_model= trainer.train()
+            scores = trainer.predict()
+            predictions = trainer.evaluate(scores.cpu().numpy())
         save_to_csv(args, data, predictions)
         
     
